54a0116a76260af3fee944bb6e471c3df2a7034d
Vitaliy Movchan <mvv.original@gmail.com>
2018-02-17 23:53:46 +0200

    added sayHello

---
+++ src/main/java/com/example/mylibrary/Feature.java
@@ -0,0 +1,12 @@
+package com.example.mylibrary;
+
+/**
+ * @author Vitaliy Movchan. Created on 17.02.18
+ */
+
+public class Feature {
+
+    public static String sayHello() {
+        return "Hello!";
+    }
+}
